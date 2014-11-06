6732e2f20061e9201ed0f77d38a551ba536f60ad
Adam Fortuna <adam@fortuna.name>
2014-11-05 22:58:56 -0500

    Use same keys for env and yml set variables

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,7 +1,7 @@
 if ENV['GOODREADS_KEY'] && ENV['GOODREADS_SECRET']
   APP_CONFIG = {
-    'key' => ENV['GOODREADS_KEY'],
-    'secret' => ENV['GOODREADS_SECRET']
+    'goodreads_key' => ENV['GOODREADS_KEY'],
+    'goodreads_secret' => ENV['GOODREADS_SECRET']
   }
 else
   APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
