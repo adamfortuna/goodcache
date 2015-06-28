464761120cba6295984b4c0ab699e96f1d88bdcb
Adam Fortuna <adam@fortuna.name>
2015-06-28 01:38:45 -0400

    Added ability to refresh a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ActionController::Base
+  before_filter :refresh_user
   respond_to :html, :json
 
   def show
     @user_id = params[:id]
-
     respond_with user
   end
 
@@ -11,5 +11,11 @@ class UsersController < ActionController::Base
 
   def user
     @user ||= User.find_or_create_by(goodreads_id: params[:id])
+  end
+
+  def refresh_user
+    if params[:refresh] || params[:reload] || (user.updated_at < 6.hours.ago)
+      user.refresh!
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,27 @@ class User < ActiveRecord::Base
   has_many :shelves, dependent: :destroy
   validates :goodreads_id, uniqueness: true
 
-  after_create :update_name
+  after_create :refresh!
 
-  private
-
-  def update_name
-    if result = goodreads.user(self.goodreads_id)
-      update_attribute(:name, result['name'])
-      result['user_shelves'].each do |shelf|
+  def refresh!
+    return true unless goodinfo
+    update_attribute(:name, goodinfo['name'])
+    goodinfo['user_shelves'].each do |shelf|
+      if shelf = shelves.find_by(shelf: shelf['name'])
+        shelf.refresh!
+      else
         shelves.find_or_create_by(shelf: shelf['name'])
       end
     end
   end
 
+  private
+
   def goodreads
     @client ||= Goodreads.new(api_key: APP_CONFIG['goodreads_key'], api_secret: APP_CONFIG['goodreads_secret'])
+  end
+
+  def goodinfo
+    @goodinfo ||= goodreads.user(self.goodreads_id)
   end
 end
